b67a192afce9a83803c78c7b10324d57856e547b
Kendall White <whitke@student.breckschool.org>
2021-05-06 14:29:26 -0500

    updated tabs

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,36 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Kendall White on 5/6/21.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        TabView {
+            Text("Featured View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
